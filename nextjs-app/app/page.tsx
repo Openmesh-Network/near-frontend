5bd5d41be46266e03ce63d3891f784ed1862a455
Plopmenz <plopmenz@gmail.com>
2025-04-09 10:54:54 +0200

    powered by xnode

--- nextjs-app/app/page.tsx
+++ nextjs-app/app/page.tsx
@@ -2,6 +2,8 @@ import { Section } from "@/components/text";
 import { DeployXnode } from "@/components/xnode/deploy-xnode";
 import { ImportXnode } from "@/components/xnode/import-xnode";
 import { MyXnodes } from "@/components/xnode/my-xnodes";
+import { Cpu } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 export default function IndexPage() {
@@ -17,6 +19,14 @@ export default function IndexPage() {
           <MyXnodes />
         </div>
       </Section>
+      <div className="w-full flex fixed bottom-2 place-content-center">
+        <Link href="https://www.openmesh.network/Xnodepage" target="_blank">
+          <div className="flex gap-1 bg-[#0c2246d6] text-white rounded-lg px-2 py-1">
+            <Cpu />
+            <span>Powered by Xnode</span>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
